6788242c3ffe804a9bd30c138fc0b542444f245f
jamesyburr <vint.james@gmail.com>
2016-06-27 12:23:08 -0400

    literally added everything else

---
+++ src/main/java/com/james/services/AnonFileRepository.java
@@ -0,0 +1,10 @@
+package com.james.services;
+
+import com.james.entities.AnonFile;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jamesyburr on 6/27/16.
+ */
+public interface AnonFileRepository extends CrudRepository<AnonFile, Integer>{
+}
